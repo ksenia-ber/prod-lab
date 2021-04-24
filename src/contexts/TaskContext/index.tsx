738c430610a1685ea221241520f2d8e7b3d88e7b
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-24 19:40:08 +0200

    feat: ➕ add useTaskContext

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
+import { noop } from "../../functions/noop";
 import { ContextReducerParams } from "../ContextReducer";
 
 export interface TaskContextType {
@@ -8,9 +9,7 @@ export interface TaskContextType {
 
 export const DEFAULT_TASK_CONTEXT_VALUE: TaskContextType = {
   title: "Task",
-  setTitle: () => {
-    // nothing
-  },
+  setTitle: noop,
 };
 
 export const TaskContext = createContext<TaskContextType>(
@@ -18,7 +17,10 @@ export const TaskContext = createContext<TaskContextType>(
 );
 
 export const reduceTaskContext = (_: ContextReducerParams) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [title, setTitle] = useState(DEFAULT_TASK_CONTEXT_VALUE.title);
 
   return { title, setTitle };
 };
+
+export const useTaskContext = () => useContext(TaskContext);
