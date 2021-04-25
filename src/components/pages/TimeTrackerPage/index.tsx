d8b66db4bb5d36b2b303877a717fb0284b19ed8b
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-25 17:49:09 +0200

    style: ➕ add styles to TimeTrackerPage 💅

--- src/components/pages/TimeTrackerPage/index.tsx
+++ src/components/pages/TimeTrackerPage/index.tsx
@@ -4,6 +4,7 @@ import { useTimerContext } from "../../../contexts/TimerContext";
 import { Button } from "../../atoms/Button";
 import { EditableLabel } from "../../atoms/EditableLabel";
 import { Time } from "../../atoms/Time";
+import "./index.css";
 
 export const TimeTrackerPage: FC = () => {
   const { title, setTitle } = useTaskContext();
@@ -13,39 +14,48 @@ export const TimeTrackerPage: FC = () => {
     <div className="time-tracker">
       {timer.status !== "stopped" && (
         <>
-          <EditableLabel text={title} setText={setTitle} />
+          <p className="time-tracker-header">
+            <EditableLabel text={title} setText={setTitle} />
+          </p>
           <Time
             seconds={seconds}
             minutes={minutes}
             hours={hours}
             size="large"
           />
+          <div className="button-container">
+            {timer.status === "idle" && (
+              <Button primary label="Start" onClick={timer.start} />
+            )}
+            {timer.status === "started" && (
+              <>
+                <Button primary label="Pause" onClick={timer.pause} />
+                <Button label="Stop" onClick={timer.stop} />
+              </>
+            )}
+            {timer.status === "paused" && (
+              <>
+                <Button primary label="Resume" onClick={timer.resume} />
+                <Button label="Stop" onClick={timer.stop} />
+              </>
+            )}
+          </div>
         </>
-      )}
-      {timer.status === "idle" && (
-        <Button primary label="Start" onClick={timer.start} />
-      )}
-      {timer.status === "started" && (
-        <Button primary label="Pause" onClick={timer.pause} />
-      )}
-      {timer.status === "paused" && (
-        <Button primary label="Resume" onClick={timer.resume} />
-      )}
-      {(timer.status === "started" || timer.status === "paused") && (
-        <Button label="Stop" onClick={timer.stop} />
       )}
       {timer.status === "stopped" && (
         <>
-          <h1>Well done!</h1>
-          <p>Today you spent</p>
+          <p className="time-tracker-header--stopped">Well done!</p>
+          <p className="time-tracker-subheader--stopped">Today you spent</p>
           <Time
             seconds={seconds}
             minutes={minutes}
             hours={hours}
             size="small"
           />
-          <p>on {title}</p>
-          <Button label="Back" onClick={timer.reset} />
+          <p className="task-title">on {title}</p>
+          <div className="button-container">
+            <Button label="Back" onClick={timer.reset} />
+          </div>
         </>
       )}
     </div>
