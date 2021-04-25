42afb93214804437cad02b3f64b28f2ae015888f
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-25 16:38:39 +0200

    fix: 🔄 imports

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import "./index.css";
 
 export interface ButtonProps {
@@ -22,7 +23,7 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC<ButtonProps> = ({
+export const Button: FC<ButtonProps> = ({
   primary = false,
   backgroundColor,
   label,
--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks  */
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { noop } from "../../functions/noop";
 import { ContextReducerParams } from "../ContextReducer";
 
