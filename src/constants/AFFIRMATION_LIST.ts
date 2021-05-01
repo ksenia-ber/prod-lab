1caa4c9ec17245851dba8345d7581f23f339ae44
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-01 14:13:41 +0200

    feat: ➕ add AFFIRMATION_LIST

---
+++ src/constants/AFFIRMATION_LIST.ts
@@ -0,0 +1,7 @@
+export const AFFIRMATION_LIST = [
+  "I am courageous and I stand up for myself.",
+  "I radiate beauty, charm, and grace.",
+  "I deserve love and I get it in abundance.",
+  "I believe I can be all that I want to be.",
+  "I deserve to be healthy and feel good.",
+];
