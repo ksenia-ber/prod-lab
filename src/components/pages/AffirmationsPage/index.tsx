8c3afe3b129df5534f44771faf37cb93d8817870
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-01 14:14:25 +0200

    fix: 🧪 randomiser in AffirmationsPage

--- src/components/pages/AffirmationsPage/index.tsx
+++ src/components/pages/AffirmationsPage/index.tsx
@@ -1,8 +1,24 @@
-import { FC } from "react";
+import { FC, useCallback, useState } from "react";
+import { AFFIRMATION_LIST } from "../../../constants/AFFIRMATION_LIST";
+import { randomiser } from "../../../functions/randomiser";
 import classes from "./index.module.css";
 
 export interface AffirmationsPageProps {}
 
 export const AffirmationsPage: FC<AffirmationsPageProps> = () => {
-  return <div className={classes.affirmations}></div>;
+  const [{ getCurrent, next }] = useState(randomiser(AFFIRMATION_LIST));
+  const [text, setText] = useState(getCurrent());
+
+  const handleClick = useCallback(() => {
+    next();
+    setText(getCurrent());
+  }, []);
+
+  return (
+    <div className={classes.affirmations}>
+      <div className={classes.background}></div>
+      <p>{text}</p>
+      <input type="button" value="Show new" onClick={handleClick} />
+    </div>
+  );
 };
