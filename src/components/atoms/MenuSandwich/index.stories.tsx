85be83fb65157163c409c065b974dcf0a7731220
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-02 18:04:25 +0200

    refactor: ➕ add className and onClick to MenuSandwich

--- src/components/atoms/MenuSandwich/index.stories.tsx
+++ src/components/atoms/MenuSandwich/index.stories.tsx
@@ -1,11 +1,17 @@
+import { action } from "@storybook/addon-actions";
 import { Story, Meta } from "@storybook/react";
-import { MenuSandwich } from ".";
+import { MenuSandwich, MenuSandwichProps } from ".";
 
 export default {
   title: "components/atoms/MenuSandwich",
   component: MenuSandwich,
-} as Meta<{}>;
+} as Meta<MenuSandwichProps>;
 
-const MenuSandwichStory: Story = () => <MenuSandwich />;
+const MenuSandwichStory: Story<MenuSandwichProps> = (props) => (
+  <MenuSandwich {...props} />
+);
 
 export const Real = MenuSandwichStory.bind({});
+Real.args = {
+  onClick: action("onClick"),
+};
--- src/components/atoms/MenuSandwich/index.tsx
+++ src/components/atoms/MenuSandwich/index.tsx
@@ -1,7 +1,16 @@
+import { FC, HTMLAttributes } from "react";
 import classes from "./index.module.css";
 
-export const MenuSandwich = () => (
-  <div className={classes.sandwich}>
+export type MenuSandwichProps = Pick<
+  HTMLAttributes<HTMLDivElement>,
+  "className" | "onClick"
+>;
+
+export const MenuSandwich: FC<MenuSandwichProps> = ({ className, onClick }) => (
+  <div
+    className={[classes.sandwich, className].filter(Boolean).join(" ")}
+    onClick={onClick}
+  >
     <div></div>
     <div></div>
     <div></div>
