d76711083b175a07a0d3caa88a1d8fd314118512
Alexey Berezin <beraliv.spb@gmail.com>
2021-05-02 18:59:10 +0200

    feat: 🔄 use Router with paths

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,33 @@ import "./index.css";
 import { reportWebVitals } from "./reportWebVitals";
 import { ContextReducer } from "./contexts/ContextReducer";
 import { TimeTrackerPage } from "./components/pages/TimeTrackerPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { IntroScreenPage } from "./components/pages/IntroScreenPage";
+import { timeToTimeOfDay } from "./functions/timeToTimeOfDay";
+import { AffirmationsPage } from "./components/pages/AffirmationsPage";
+import { Menu } from "./components/molecules/Menu";
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextReducer>
-      <TimeTrackerPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Menu />
+            <IntroScreenPage
+              timeOfDay={timeToTimeOfDay(new Date().getTime())}
+            />
+          </Route>
+          <Route exact path="/affirmations">
+            <Menu />
+            <AffirmationsPage />
+          </Route>
+          <Route exact path="/time-tracker">
+            <Menu />
+            <TimeTrackerPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ContextReducer>
   </React.StrictMode>,
   document.getElementById("root")
